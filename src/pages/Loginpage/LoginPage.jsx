93088f3fc2283ba1260bb3619111ad19837aaf20
lost-rider <amisha1324s@gmail.com>
2025-03-19 23:47:28 +0530

    Initial Chnages

---
+++ src/pages/Loginpage/LoginPage.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import "./LoginPage.css"
+import bot from "../../Assets/bot.png";
+
+const LoginPage = () => {
+  return (
+    <div className="wrapper fadeInDown">
+      <div id="formContent">
+        <div className="fadeIn first">
+          <img
+            src={bot}
+            id="icon"
+            alt="User Icon"
+          />
+        </div>
+
+        <form>
+          <input
+            type="text"
+            id="login"
+            className="fadeIn second"
+            name="username"
+            placeholder="employeeId"
+          />
+          <input
+            type="text"
+            id="login"
+            className="fadeIn second"
+            name="username"
+            placeholder="password"
+          />
+          
+          {/* <input
+            type="password"
+            id="password"
+            className="fadeIn second"
+            name="password"
+            placeholder="Password"
+          /> */}
+          <input type="submit" className="fadeIn fourth btn btn-primary" value="Log In" />
+        </form>
+
+        <div id="formFooter">
+          <a className="underlineHover" href="#">
+            Forgot Password?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
