b2649e86f4eac948dacba9405dd4e0258cf9303f
Aarushi1623 <aarushisharma1623@gmail.com>
2025-03-21 01:19:24 +0530

    spelling error corrected

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
-import "./AdminnPage.css"
+import "./AdminPage.css"
 import bot from "../../Assets/bot.png";
 // import Button from "../../components/samplecomponent";
 
