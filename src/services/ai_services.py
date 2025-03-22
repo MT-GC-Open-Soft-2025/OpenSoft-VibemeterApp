2fc98fe0ab7e10f5031457c971b82ccb5aa9faab
Dami-18 <damiyagondha@gmail.com>
2025-03-23 00:42:52 +0530

    add gemini model

--- src/services/ai_services.py
+++ src/services/ai_services.py
@@ -1,7 +1,15 @@
 import torch
-
+import os
+from dotenv import load_dotenv
 from transformers import pipeline
 from transformers import BartForConditionalGeneration, BartTokenizer
+import google.generativeai as genai
+
+load_dotenv()
+GEMINI_KEY = os.getenv("GEMINI_KEY")
+
+genai.configure(api_key=GEMINI_KEY)
+chatmodel = genai.GenerativeModel("gemini-1.5-flash")
 
 # Load the sentiment analysis pipeline
 sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
@@ -9,15 +17,6 @@ model_name = "facebook/bart-large-cnn"
 tokenizer = BartTokenizer.from_pretrained(model_name)
 model = BartForConditionalGeneration.from_pretrained(model_name)
 
-
-# model_id = "meta-llama/Llama-3.3-70B-Instruct"
-
-# pipeline = transformers.pipeline(
-#     "text-generation",
-#     model=model_id,
-#     model_kwargs={"torch_dtype": torch.bfloat16},
-#     device_map="auto",
-# )
 
 def analyze_response(question: str, answer: str):
     """Formats question and answer into a structured sentence, 
@@ -32,17 +31,8 @@ def analyze_response(question: str, answer: str):
 
 # generates a response and returns it 
 def generate_response(user_input:str) -> str:
-    messages = [
-        {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
-        {"role": "user", "content": user_input},
-    ]
-    outputs = pipeline(
-    messages,
-    max_new_tokens=256,
-    )
-    response = outputs[0]["generated_text"][-1]
-
-    return response
+    response = chatmodel.generate_content(user_input)
+    return response.text
 
 
 def summarize_text(text, max_length=130, min_length=30, length_penalty=2.0):
@@ -51,3 +41,6 @@ def summarize_text(text, max_length=130, min_length=30, length_penalty=2.0):
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     
     return summary
+
+if __name__=="__main__":
+    print(generate_response("I am feeling stressed"))
