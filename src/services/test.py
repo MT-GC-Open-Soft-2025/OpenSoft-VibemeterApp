c98b33c99243fedaadf4eea56638dff8f10fa7de
Dami-18 <damiyagondha@gmail.com>
2025-03-24 01:01:39 +0530

    minor changes

--- src/services/test.py
+++ src/services/test.py
@@ -8,7 +8,6 @@ from ai_services import initialize as initi
 
 def chat_controller(user_input,chatObj):
     result = generate_service(user_input,chatObj)
-    # print(result)
     return result
 
 if __name__=="__main__":
@@ -18,9 +17,17 @@ if __name__=="__main__":
     res = chat_controller("Hi I am feeling happy",chatObj)
 
     print(res)
-    print("PRINTING CHAT---------------------")
+    print("---------PRINTING CHAT-------------")
     
     print("-------------START----------------")
-    for message in chatObj.get_history():
-        print(message.parts[0].text)
+    # print(chat_controller("Summarize our discussion from my perspective, focusing on key points, decisions, and unresolved questions.",chatObj))
+    # print("The type of chatObj.get_history() is ",type(chatObj.get_history()))
+    # for message in chatObj.get_history():
+    #     print(message.parts[0].text)
+    print(chatObj.get_history())
 
+# For chat history
+# Whenever user clicks on end chat, we will store whole chat object in database which is basically a list only, then we can retrieve info from it as required
+# at the end, do parsing and convert it to the specific form of message model stored in database
+# also, can keep some token or flag something which indicates that current chat object is still alive so, we can fetch from it using frontend
+# if it is destroyed, then 
