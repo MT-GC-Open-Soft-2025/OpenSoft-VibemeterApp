751c2c7ce941a382dd97640f905f10c9fa1f815e
Prasanthi-Peram <prasanthiperam29@gmail.com>
2025-03-24 20:01:30 +0000

    fix: Use try and except

--- src/controllers/chat_controller.py
+++ src/controllers/chat_controller.py
@@ -17,13 +17,18 @@ async def response_controller(payload,user) -> Dict[str, Any]:
    convid= payload.convid
    message= payload.message
    chatObj= payload.chatObj
+    
+try:
+        chat_record = await Chat.find(Chat.convid == convid).first_or_none()
 
-   chat_record = await Chat.find(Chat.convid==convid).first_or_none()
-   if not chat_record:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not retrive chat session"
-        )
-   
-   response= await send_response(user,message,convid, chatObj)
-   return response
+        if not chat_record:
+            raise HTTPException(
+                status_code=404,
+                detail="Could not retrieve chat session"
+            )
+        
+        response = await send_response(user, message, convid, chatObj)
+        return response
+        
+except Exception as error:
+            raise HTTPException(status_code=400, detail=str(error))
