01c6dc41f2541b611bf0684e2b999a45047a84d9
GayathriVadapalli <gayathrisphg21@gmail.com>
2025-03-24 23:52:47 +0530

    changed items in the sidebar and added searchbar

---
+++ src/components/Adminpagenavbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Adminpagenavbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-content">
+        <div className="admin-name">Admin_Name</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -4,15 +4,29 @@ import "./AdminPage.css";
 import bot from "../../Assets/bot.png";
 import EmotionZoneChart from "./EmotionZone";
 import PieChart from "./PieChart";
+import Navbar from "../../components/Adminpagenavbar";
+import Sidebar from "../../components/Adminpagesidebar";
+import Searchbar from "../../components/Adminpagesearchbar";
 // import Button from "../../components/samplecomponent";
 
 const AdminPage = () => {
   return (
     <>
-      <div className="wrapper fadeInDown">"This is admin page."</div>
-      <div className="charts">
-        <EmotionZoneChart />
-        <PieChart />
+      <Navbar />
+      <Sidebar />
+      <div style={{ 
+        marginLeft: '200px', 
+        marginTop: '64px',
+        backgroundColor: 'white',
+        minHeight: '100vh',
+        padding: '20px'
+      }}>
+        <Searchbar />
+        <div className="wrapper fadeInDown">"This is admin page."</div>
+        <div className="charts">
+          <EmotionZoneChart />
+          <PieChart />
+        </div>
       </div>
     </>
   );
