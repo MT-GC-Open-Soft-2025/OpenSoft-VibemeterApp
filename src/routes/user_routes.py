53b288354daee1b4b3709fe44c3a0e6860f80f35
Dami-18 <damiyagondha@gmail.com>
2025-03-28 01:19:38 +0530

    add convoids end point

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from typing import Any, Dict
-from src.controllers.user_controller import user_controller
+from src.controllers.user_controller import user_controller, convoid_controller
 from src.middlewares.authmiddleware import authenticate
 
 user_router = APIRouter()
@@ -9,6 +9,10 @@ user_router = APIRouter()
 @user_router.get("/getUserDetails")
 async def get_user_route(user: Dict[str, Any] = Depends(authenticate)):  
     return await user_controller(user)
+
+@user_router.get("/getConvoids")
+async def get_convoids_route(user: Dict[str, Any] = Depends(authenticate)):
+    return await convoid_controller(user)
 
 
 
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,5 +1,7 @@
 from src.models.employee import Employee
+from src.models.chats import Chat
 from fastapi import HTTPException, status
+from typing import List
 
 async def get_user_details(emp_id: str):
     user = await Employee.find(Employee.emp_id == emp_id).first_or_none()
@@ -13,6 +15,21 @@ async def get_user_details(emp_id: str):
             "vibe_score" : user.vibe_score,
             "factors_in_sorted_order" : user.factors_in_sorted_order,
         }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found."
+        )
+    
+async def get_all_convoid(emp_id: str):
+    matched_chats = await Chat.find(Chat.empid==emp_id).to_list()
+
+    if matched_chats:
+        convid_list: List[str] = [chat.convid for chat in matched_chats]
+        return {
+           "convid_list": convid_list,
+        }
+
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
