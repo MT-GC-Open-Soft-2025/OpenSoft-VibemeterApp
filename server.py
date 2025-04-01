ab7b27add8f5f760ce98f585b5452d9b296e5c46
Tejal Saurabhi <tejalsaurabhi1302@gmail.com>
2025-04-01 22:55:30 +0530

    changed requirements for mac

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  #Import CORS middleware
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
@@ -8,7 +9,6 @@ from datetime import datetime, timezone
 from src.routes.auth_routes import auth_router
 from src.routes.user_routes import user_router  
 from src.routes.chat_routes import chat_router
-
 from src.routes.admin_routes import admin_router
 import os
 
@@ -16,14 +16,11 @@ load_dotenv()
 
 uri = os.getenv("MONGO_URI")
 
-
 client = MongoClient(uri, server_api=ServerApi("1"))
-
 
 try:
     client.admin.command("ping")
     print("Pinged your deployment. You successfully connected to MongoDB!")
-
 except Exception as e:
     print(e)
 
@@ -37,14 +34,21 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+#Add CORS middleware here
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  #your frontend's origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(user_router, prefix="/user", tags=["User"])
 app.include_router(admin_router, prefix="/admin", tags=["Admin"])
 app.include_router(chat_router, prefix="/chat", tags=["Chat"])
 
 
-
 @app.get("/")
 def home():
     return {"message": "Backend Running"}
-
