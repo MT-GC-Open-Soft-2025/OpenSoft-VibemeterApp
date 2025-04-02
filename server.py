7665409c43bb85b8b30a57c0c5c8e9acc7809c36
soumili202 <soumilirupsa2020@gmail.com>
2025-04-02 23:02:15 +0530

    Final no changes in main after this

--- server.py
+++ server.py
@@ -45,7 +45,7 @@ app = FastAPI(lifespan=lifespan)
 #Add CORS middleware here
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  #your frontend's origin
+    allow_origins=["http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
